798a365cf874f984b93457d9fd865a9cc5e2b9ab
Sam Welsh <welsh.271@osu.edu>
2020-05-05 14:08:54 -0400

    fisher-bookstore-api

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -20,53 +20,8 @@ namespace Fisher.Bookstore.Controllers
         public IActionResult GetAll()
         {
             return Ok(booksRepository.GetBooks());
-        }
+        }  
 
-        [HttpGet("{bookId}")]
-        public IActionResult Get(int bookId)
-        {
-            if (!booksRepository.BookExists(bookId))
-            {
-                return NotFound();
-            }
-
-            return Ok(booksRepository.GetBook(bookId));
-        }
-
-        [HttpPost]
-        public IActionResult Post([FromBody]Book book)
-        {
-            var bookId = booksRepository.AddBook(book);
-            return Created($"https://localhost:5001/api/books/{bookId}", book);
-        }
-
-        [HttpPut("{bookId}")]
-        public IActionResult Put(int bookId, [FromBody] Book book)
-        {
-            if (bookId != book.Id)
-            {
-                return BadRequest();
-            }
-
-            if (!booksRepository.BookExists(bookId))
-            {
-                return NotFound();
-            }
-
-            booksRepository.UpdateBook(book);
-            return Ok(book);
-        }
-
-        [HttpDelete("{bookId}")]
-        public IActionResult Delete(int bookId)
-        {
-            if (!booksRepository.BookExists(bookId))
-            {
-                return NotFound();
-            }
-
-            booksRepository.DeleteBook(bookId);
-            return Ok();
-        }
     }
+      
 }
